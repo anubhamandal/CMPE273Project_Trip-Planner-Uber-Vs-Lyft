df7ac9bacc6b35ffce300f4bb7ca384d6d1852e4
Aditi Garg <aditi.garg@sjsu.edu>
2016-11-30 17:34:26 -0800

    initial commit

---
+++ locationusingpython/google_results.py
@@ -0,0 +1,69 @@
+{
+   "results" : [
+      {
+         "address_components" : [
+            {
+               "long_name" : "3700",
+               "short_name" : "3700",
+               "types" : [ "street_number" ]
+            },
+            {
+               "long_name" : "Casa Verde Street",
+               "short_name" : "Casa Verde St",
+               "types" : [ "route" ]
+            },
+            {
+               "long_name" : "North San Jose",
+               "short_name" : "North San Jose",
+               "types" : [ "neighborhood", "political" ]
+            },
+            {
+               "long_name" : "San Jose",
+               "short_name" : "San Jose",
+               "types" : [ "locality", "political" ]
+            },
+            {
+               "long_name" : "Santa Clara County",
+               "short_name" : "Santa Clara County",
+               "types" : [ "administrative_area_level_2", "political" ]
+            },
+            {
+               "long_name" : "California",
+               "short_name" : "CA",
+               "types" : [ "administrative_area_level_1", "political" ]
+            },
+            {
+               "long_name" : "United States",
+               "short_name" : "US",
+               "types" : [ "country", "political" ]
+            },
+            {
+               "long_name" : "95134",
+               "short_name" : "95134",
+               "types" : [ "postal_code" ]
+            }
+         ],
+         "formatted_address" : "3700 Casa Verde St, San Jose, CA 95134, USA",
+         "geometry" : {
+            "location" : {
+               "lat" : 37.4129401,
+               "lng" : -121.9388528
+            },
+            "location_type" : "ROOFTOP",
+            "viewport" : {
+               "northeast" : {
+                  "lat" : 37.4142890802915,
+                  "lng" : -121.9375038197085
+               },
+               "southwest" : {
+                  "lat" : 37.4115911197085,
+                  "lng" : -121.9402017802915
+               }
+            }
+         },
+         "place_id" : "ChIJZ6iyDwXJj4AR_P76Ndi2x5A",
+         "types" : [ "street_address" ]
+      }
+   ],
+   "status" : "OK"
+}
