1d4894fae7bf39990f00d0724bc093fbe5b82e23
bhusariketki <bhusariketki@gmail.com>
2016-12-07 16:08:18 -0800

    updated

--- python/model.py
+++ python/model.py
@@ -4,9 +4,6 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from datetime import datetime
 
-
-
-
 # AWS RDS Database Configurations details
 app = Flask(__name__)
 DATABASE = ''
@@ -49,6 +46,24 @@ class Location(db.Model):
 		
 	def __repr__(self):
 		return '<location %r>' % self.name
+
+        
+class Trips(db.Model):
+
+	# Data Model Trips Table
+        id = db.Column(db.Integer, primary_key=True)
+        start = db.Column(db.Integer, unique=False)
+        others = db.Column(db.String(50), unique=False)
+        end = db.Column(db.Integer , unique=False)
+
+	def __init__(self,start,end,others):
+		# initialize columns
+		self.start = start
+		self.others = others
+		self.end = end
+		
+	def __repr__(self):
+		return '<location %r>' % self.start
 
 ## db.execute executes sql query like CREATE DATABASE IF NOT EXISTS IN THE DATABASE
 
